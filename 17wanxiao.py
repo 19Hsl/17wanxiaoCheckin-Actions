096ca7d332bbcd8149b4eb58d12eb165a27b0b89
19Hsl <59008669+19Hsl@users.noreply.github.com>
2020-09-10 13:14:26 +0800

    Update 17wanxiao.py

--- 17wanxiao.py
+++ 17wanxiao.py
@@ -13,18 +13,18 @@ jsons = {
 			"deptid": 71881,
 			"text": "信息科学与工程学院-计算机科学与技术(*)-计科F1705"
 		},
-		"areaStr": "{\"streetNumber\":\"\",\"street\":\"莲花街辅路\",\"district\":\"中原区\",\"city\":\"郑州市\",\"province\":\"河南省\",\"town\":\"\",\"pois\":\"河南工业大学(莲花街校区)\",\"lng\":113.55341700000157,\"lat\":34.83379196292185,\"address\":\"中原区莲花街辅路河南工业大学(莲花街校区)\",\"text\":\"河南省-郑州市\",\"code\":\"\"}",
-		"reportdate": round(time.time()*1000),
+		"areaStr": "{\"streetNumber\":\"\",\"street\":\"\",\"district\":\"中原区\",\"city\":\"郑州市\",\"province\":\"河南省\",\"town\":\"\",\"pois\":\"河南工业大学(莲花街校区)学生公寓C区-3座\",\"lng\":113.56038400000175,\"lat\":34.83966696227574,\"address\":\"中原区河南工业大学(莲花街校区)学生公寓C区-3座\",\"text\":\"河南省-郑州市\",\"code\":\"\"}",
+		"reportdate": 1599714391463,
 		"customerid": 43,
 		"deptid": 71881,
 		"source": "app",
 		"templateid": "clockSign2",
-		"stuNo": "201716010501",
-		"username": "梅杨",
-		"userid": 13604407,
+		"stuNo": "201716010505",
+		"username": "化帅磊",
+		"userid": 6615274,
 		"updatainfo": [{
 			"propertyname": "temperature",
-			"value": "35.8"
+			"value": "36.1"
 		}, {
 			"propertyname": "symptom",
 			"value": "无症状"
@@ -32,7 +32,7 @@ jsons = {
 		"customerAppTypeRuleId": 147,
 		"clockState": 0
 	},
-	"token": "270c27a3-594b-4ca1-a4f5-7b411660e502"
+	"token": "adae2729-ab94-4cd6-88fb-4d3d92993738"
 }
 
 nowhour = datetime.datetime.now().hour
@@ -45,56 +45,3 @@ response = requests.post(check_url, json=jsons)
 
 res = json.dumps(response.json(), sort_keys=True, indent=4, ensure_ascii=False)
 print(res)
-
-
-SCKEY = "SCU112803Td38eb92d00cfce7b66d8c1d48415edd25f5796cb03f08"
-
-now_time = datetime.datetime.now()
-bj_time = now_time + datetime.timedelta(hours=8)
-
-test_day = datetime.datetime.strptime('2020-12-19 00:00:00','%Y-%m-%d %H:%M:%S')
-date = (test_day - bj_time).days
-desp = f"""
-------
-### 现在时间：
-```
-{bj_time.strftime("%Y-%m-%d %H:%M:%S %p")}
-```
-### 打卡信息：
-```
-{res}
-```
-> 关于打卡信息
->
-> 1、成功则打卡成功
->
-> 2、系统异常则是打卡频繁
-
-### ⚡考研倒计时:
-```
-{date}天
-```
-
->
-> [GitHub项目地址](https://github.com/ReaJason/17wanxiaoCheckin-Actions) 
->
->期待你给项目的star✨
-"""
-
-headers = {
-    "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-}
-
-send_url = f"https://sc.ftqq.com/{SCKEY}.send"
-
-params = {
-    "text": f"完美校园健康打卡---{bj_time.strftime('%H:%M:%S')}",
-    "desp": desp
-}
-    
-# 发送消息
-response = requests.post(send_url, data=params, headers=headers)
-if response.json()["errmsg"] == 'success':
-        print("Server酱推送服务成功")
-else:
-        print("Something Wrong")
